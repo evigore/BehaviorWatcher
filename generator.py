fccbd304067cee05d08ef3368e42939b8603073a
evgenyPro <evgeny_net_x@protonmail.com>
2021-08-12 21:10:12 +0700

    added generator of users

--- generator.py
+++ generator.py
@@ -1,14 +1,37 @@
-{
-	name:
-	task_id:
-	language_id: 
-	failed_test_num:
-	last_login_time:
-	test_passed:
-	stole_from: []
-	reputation:
-	year
-	faculty_id
-	university_id
-}
+import random
+import json
+from datetime import date
 
+def generate_users():
+	random.seed(1234)
+	users = []
+
+	for i in range(5000):
+		user = {
+			'id': i,
+			'task_id': random.randrange(3),
+			'language_id': random.randrange(0, 3),
+			'last_login_time': date(year=2021-random.randrange(2), month=8-random.randrange(2), day=1+random.randrange(25)),
+			'test_passed': bool(random.randrange(1)),
+			'stole_from': [random.randrange(1000) for i in range(random.randrange(4))],
+			'reputation': int(random.random()*100),
+			'course_year': random.randrange(4)+1,
+			'faculty_id': random.randrange(3),
+			'university_id': random.randrange(3)
+		}
+
+		users.append(user)
+
+	return users
+
+def save_users_to_file(filename, users):
+	f = open(filename + '.json', 'w')
+	f.write(json.dumps(users, indent=4, sort_keys=True, default=str))
+	f.close()
+
+def load_users_from_file(filename):
+	f = open(filename + '.json', 'r')
+	users = json.load(f)
+	f.close()
+
+	return users
