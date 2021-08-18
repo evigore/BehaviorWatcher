233c6af9a1135dc02992549e6ce52747498f7b06
Adilet Soronov <adiletelf@gmail.com>
2021-08-18 14:38:35 +0700

    disable 'SQLALCHEMY_TRACK_MODIFICATIONS'

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,10 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
