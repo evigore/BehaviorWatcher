4bbb9d2d40300a9cc39cb5db052d8d9bc2f9f16e
Adilet Soronov <adiletelf@gmail.com>
2021-08-18 14:30:31 +0700

    create 'backend' folder

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, abort, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+
+
+@app.before_request
+def only_json():
+    if not request.is_json:
+        abort(400)
+
+    
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
