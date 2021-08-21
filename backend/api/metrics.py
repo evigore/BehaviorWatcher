288215acc428e111eff880e0a53a96c76009e9bd
Adilet Soronov <adiletelf@gmail.com>
2021-08-21 19:11:30 +0700

    add GET /metrics to swagger.yml

--- backend/api/metrics.py
+++ backend/api/metrics.py
@@ -1,2 +1,11 @@
 def read_all():
-    return "metrics.read_all()"
+    return [
+        {
+            "id": 0,
+            "userId": 0,
+            "taskId": 0,
+            "taskCopied": True,
+            "taskViewed": True,
+            "readingTime": 0
+        }
+    ]
--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,8 @@ from api import config
 
 
 connex_app = config.connex_app
-connex_app.add_api('swagger.yml')
+connex_app.add_api('swagger.yml')  # uses ./api/swagger.yml
+
 
 if __name__ == '__main__':
     connex_app.run(debug=True)
