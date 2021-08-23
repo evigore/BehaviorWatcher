9175f663872539d06653644c3b0f573f1c4ba73a
Adilet Soronov <adiletelf@gmail.com>
2021-08-24 02:37:25 +0700

    Implement unique constraint for columns

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,9 @@ from config import db
 
 
 class Metric(db.Model):
+    # __tablename__ = 'metric'
+
+    # while constructing new object don't forget to include required key parameters
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     task_id = db.Column(db.Integer, nullable=False)
@@ -13,16 +16,24 @@ class Metric(db.Model):
     senders = db.relationship('Sender', backref='metric', lazy=True)
     receivers = db.relationship('Receiver', backref='metric', lazy=True)
 
+    # Unique pair of (user_id, task_id)
+    __table_args__ = (db.UniqueConstraint('user_id', 'task_id', name='_user_task_uc'), )  # tuple
+
+
 
 class Sender(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'), nullable=False)
-    # user_id belongs to self, not to self.metric.user_id
     user_id = db.Column(db.Integer, nullable=False)
+    # user_id belongs to self, not to self.metric.user_id
+    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'))# 
+
+    __table_args__ = (db.UniqueConstraint('user_id', 'metric_id', name='_user_metric_uc'), )  # tuple
 
 
 class Receiver(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'), nullable=False)
-    # user_id belongs to self, not to self.metric.user_id
     user_id = db.Column(db.Integer, nullable=False)
+    # user_id belongs to self, not to self.metric.user_id
+    metric_id = db.Column(db.Integer, db.ForeignKey('metric.id'))
+
+    __table_args__ = (db.UniqueConstraint('user_id', 'metric_id', name='_user_metric_uc'), )  # tuple
