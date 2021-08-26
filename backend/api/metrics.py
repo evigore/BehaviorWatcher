6c2af2ef6a3a3560713316aee65dccd7db6a6246
Adilet Soronov <adiletelf@gmail.com>
2021-08-26 16:58:03 +0700

    Implement logic for '/metrics/{metricId}/receivers' and '/metrics/{metricId}/senders'

--- backend/api/metrics.py
+++ backend/api/metrics.py
@@ -28,15 +28,15 @@ def read_all(user_id=None, task_id=None):
     """
     if user_id is None and task_id is None:
         metrics = Metric.query.all()
-        return metrics_schema.dump(metrics)
+        return metrics_schema.dump(metrics), 200
 
     if user_id is None:
         metrics = Metric.query.filter(Metric.task_id == task_id).all()
-        return metrics_schema.dump(metrics)
+        return metrics_schema.dump(metrics), 200
 
     if task_id is None:
         metrics = Metric.query.filter(Metric.user_id == user_id).all()
-        return metrics_schema.dump(metrics)
+        return metrics_schema.dump(metrics), 200
 
 
 def create(metric):
@@ -65,8 +65,6 @@ def create(metric):
 
         return metric_schema.dump(new_metric), 201
 
-        # return make_response(f"Successfully created metric", 201)
-
 
 def read_one(metricId):
     """
@@ -84,7 +82,7 @@ def read_one(metricId):
     if metric is None:
         abort(404, f"Metric with metricId: {metricId} does not exists.")
     else:
-        return metric_schema.dump(metric)
+        return metric_schema.dump(metric), 200
 
 
 def patch(metricId, metricData):
@@ -145,27 +143,98 @@ def read_receivers(metricId):
     metric = Metric.query.filter(Metric.id == metricId).one_or_none()
     if metric is not None:
         receivers = Receiver.query.with_parent(metric).all()
-        return receivers_schema.dump(receivers)
+        return receivers_schema.dump(receivers), 200
     else:
         abort(404, f"Metric with id {metricId} not found.")
 
 
 
-def create_receiver(metricId, receiver: '[2, 7]'):
+def create_receiver(metricId, receiverData):
     """
     Respond to a POST request for /api/metrics/{metricId}/receivers
     Add new receiver to concrete metric
 
-    :param metricId    Id of the metric to update
-    :param receiver    Array of 2 numbers (metricId, receiverId) 
+    :param metricId             Id of the metric to update
+    :param receiverData         Object with 1 property (user_id)
     :return 201 on success, 409 if metric already exists
     """
     metric = Metric.query.filter(Metric.id == metricId).one_or_none()
-    if metric is not None:
-        abort(
-            409, f"Metric with user_id: {metric['user_id']} and task_id: {metric['task_id']} already exists."
-        )
-    else:
-        pass  # TODO
 
+    if metric is None:
+        abort(
+            409, f"Metric with metricId: {metricId} doesn't exists."
+        )
+    
+    user_id = receiverData.get('user_id')
+    receiver = (Receiver.query
+        .with_parent(metric)
+        .filter(Receiver.user_id == user_id)
+        .filter(Receiver.metric_id == metricId)
+        .one_or_none()
+    )
+
+    if receiver is not None:
+        abort(
+            409, f"Metric {metricId} already has receiver with user_id: {user_id}, metric_id: {metricId}"
+        )
+
+    receiver = Receiver(user_id=user_id, metric_id=metricId)
+    metric.receivers.append(receiver)
+    db.session.add(receiver)
+    db.session.commit()
+    return receiverData, 201
+
+
+def read_senders(metricId):
+    """
+    Respond to a GET request for /api/metrics/{metricId}/senders
+    Return array of senders
+
+    :param metricId                         Id of the metric to update
+    :return 200|204 on success
+    """
+    metric = Metric.query.filter(Metric.id == metricId).one_or_none()
+    if metric is not None:
+        senders = Sender.query.with_parent(metric).all()
+        return senders_schema.dump(senders)
+    else:
+        abort(404, f"Metric with id {metricId} not found.")
+
+
+
+
+def create_sender(metricId, senderData):
+    """
+    Respond to a POST request for /api/metrics/{metricId}/sender
+    Add new receiver to concrete metric
+
+    :param metricId         Id of the metric to update
+    :param senderData       Object with 1 property (user_id) 
+    :return 201 on success, 409 if metric already exists
+    """
+    metric = Metric.query.filter(Metric.id == metricId).one_or_none()
+
+    if metric is None:
+        abort(
+            409, f"Metric with metricId: {metricId} doesn't exists."
+        )
+    
+    user_id = senderData.get('user_id')
+    sender = (Sender.query
+        .with_parent(metric)
+        .filter(Sender.user_id == user_id)
+        .filter(Sender.metric_id == metricId)
+        .one_or_none()
+    )
+
+    if sender is not None:
+        abort(
+            409, f"Metric {metricId} already has sender with user_id: {user_id}, metric_id: {metricId}"
+        )
+
+    sender = Sender(user_id=user_id, metric_id=metricId)
+    metric.senders.append(sender)
+    db.session.add(sender)
+    db.session.commit()
+    return senderData, 201
 
